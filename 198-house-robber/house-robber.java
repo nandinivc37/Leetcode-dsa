2672dfd32b666f3d100a09a180de512908e89564
Nandini V C <149227694+nandinivc37@users.noreply.github.com>
2026-09-05 00:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (37.86%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        int prev2 = 0; 
+        int prev1 = 0;
+        for (int num : nums) {
+            int temp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = temp;
+        }
+        
+        return prev1;
+    }
+}
