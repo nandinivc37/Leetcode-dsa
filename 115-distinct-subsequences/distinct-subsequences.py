10d9a3809bf1f8bd1f371ee02fa5d0f876e11d73
Nandini V C <149227694+nandinivc37@users.noreply.github.com>
2026-09-06 22:45:45 +0530

    Time: 1 ms (99.88%) | Memory: 19.2 MB (99.54%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,4 +1,11 @@
 class Solution:
-    def numDistinct(self, s, t):
-        f = cache(lambda i,j:j==-1 if -1 in (i,j) else f(i-1,j)+(s[i]==t[j])*f(i-1,j-1))
-        return f(len(s)-1,len(t)-1)
+    def numDistinct(self, s: str, t: str) -> int:
+        slen, tlen = len(s), len(t)
+        dp = [0] * tlen + [1]
+        for i in range(slen - 1, -1, -1):
+            start_j = max(0, tlen - slen + i)
+            end_j = min(tlen - 1, i)
+            for j in range(start_j, end_j + 1):
+                if s[i] == t[j]:
+                    dp[j] += dp[j + 1]
+        return dp[0]
