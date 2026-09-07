85505454303e0e0d1d474c44f87b0dd775797cc0
Nandini V C <149227694+nandinivc37@users.noreply.github.com>
2026-09-07 22:57:57 +0530

    Time: 3 ms (100.00%) | Memory: 42.9 MB (90.40%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,18 +1,19 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-       int[] dp = new int[amount + 1];
+    static {
+        for (int i = 0; i<300; i++) {
+            change(0, new int[]{});
+        }
+    }
+    public static int change(int amount, int[] coins) {
         
-        
+        int[] dp = new int[amount+1];
         dp[0] = 1;
-
-       
-        for (int coin : coins) {
-            
-            for (int i = coin; i <= amount; i++) {
-                dp[i] += dp[i - coin];
+        // find coin  combination upto amout
+        for(int coin : coins){
+            for(int i=coin; i<=amount; i++){
+                dp[i] += dp[i-coin];
             }
         }
-
         return dp[amount];
     }
 }
