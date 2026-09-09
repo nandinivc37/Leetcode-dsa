093a354d8108a8fc1af5221fd49e6fa568a01d4d
Nandini V C <149227694+nandinivc37@users.noreply.github.com>
2026-09-09 22:48:13 +0530

    Time: 1 ms (100.00%) | Memory: 52.4 MB (45.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,33 +1,39 @@
 class Solution {
+int rows, cols;
+ int island=0;
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
+        rows = grid.length;
+        cols = grid[0].length;
+       
+        for(int row =0; row < rows; row++){
+        check(grid, row);
+        }
+         return island;
+    
+    }
+        public void check(char[][] grid, int row){
+            char[] finalRow = grid[row];
+            for(int col = 0; col < cols; ++col){
+                if(finalRow[col]== '1'){
+                    bfs(grid, row, col);
+                    ++island;
 
-        int res = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1') {
-                    res++;              // 1. Increment island count
-                    dfs(grid, r, c);     // 2. Call DFS to sink the connected island
                 }
             }
         }
-
-        return res;                      // 3. Return total count after loops finish
-    }
-
-    public void dfs(char[][] grid, int r, int c) {
-        // Added missing closing parenthesis for the if-condition
-        if ((r < 0) || (r >= grid.length) || (c < 0) || (c >= grid[0].length) || (grid[r][c] == '0')) {
-            return;
+        public void bfs(char[][] grid, int row, int col){
+            grid[row][col] = '*';
+            if(row > 0 && grid[row-1][col]=='1'){
+                bfs(grid, row-1, col);
+}
+            if(row + 1 < rows && grid[row+1][col]=='1'){
+                bfs(grid, row+1, col);
+            }
+            if( col>0 && grid[row][col-1]=='1'){
+                bfs(grid, row , col-1);
+            }
+            if(col + 1 < cols && grid[row][col+1]=='1'){
+                bfs(grid, row, col+1);
+            }
         }
-
-        grid[r][c] = '0';
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-    }
 }
